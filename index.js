c94879efbdfcdb449fbe0255955005413168517a
JeremyKungu <jeremikungu27@gmail.com>
2022-08-23 14:09:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+const { arrayContaining } = require("expect");
+
+function findMatching(array, string) {
+   return  array.filter(function(element, index, arr){
+        return element.toLowerCase() === string.toLowerCase()
+    })
+
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(function(element, index) {
+        return element.startsWith(string);
+    })
+}
+
+function matchName(array, string) {
+    return array.filter(function(element, index) {
+        return element.name.toLowerCase() === string.toLowerCase()
+    })
+}
+
+
+
+
+
